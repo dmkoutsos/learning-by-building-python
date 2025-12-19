a34a45b9749a54b739a30ac9d770f38882299460
Dimitrios Koutsos <80091264+dmkoutsos@users.noreply.github.com.>
2025-12-19 22:40:26 +0200

    List directory entries and filter directories

--- tools/01_files_stats/file_stats.py
+++ tools/01_files_stats/file_stats.py
@@ -1,3 +1,8 @@
 # File stats tool
 # TODO: explore directory and collect statistics
 
+
+from pathlib import Path 
+for entry in Path("/home/dimitrios/Documents/learning-by-building-python/").iterdir(): 
+    if entry.is_dir():
+        print(entry.name)
