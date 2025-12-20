c756db228cb3cddc8bde4e4ca305f20036fbefd5
Dimitrios Koutsos <80091264+dmkoutsos@users.noreply.github.com.>
2025-12-20 23:42:05 +0200

    List directories and count them correctly

--- tools/01_files_stats/file_stats.py
+++ tools/01_files_stats/file_stats.py
@@ -2,7 +2,10 @@
 # TODO: explore directory and collect statistics
 
 
-from pathlib import Path 
+from pathlib import Path
+found = 0 
 for entry in Path("/home/dimitrios/Documents/learning-by-building-python/").iterdir(): 
-    if entry.is_dir():
-        print(entry.name)
+	if entry.is_dir():
+		print(entry.name)
+		found +=1
+print (found)
