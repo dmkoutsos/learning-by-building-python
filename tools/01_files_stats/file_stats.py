476b5fec4401999190d018f4a6a8061a5e3787dd
Dimitrios Koutsos <80091264+dmkoutsos@users.noreply.github.com.>
2025-12-22 22:57:41 +0200

    Recursive directory traversal with stack (ignore.git)

--- tools/01_files_stats/file_stats.py
+++ tools/01_files_stats/file_stats.py
@@ -3,9 +3,13 @@
 
 
 from pathlib import Path
-found = 0 
-for entry in Path("/home/dimitrios/Documents/learning-by-building-python/").iterdir(): 
-	if entry.is_file():
-		print(entry.name)
-		found +=1
-print (f"Total files: {found}")
+root = Path(".")
+to_visit = [root]
+while to_visit:
+	current_dir = to_visit.pop()
+	print(current_dir)
+	for entry in current_dir.iterdir():
+		if (entry.is_dir() and entry.name!=".git"):
+			print (entry)
+			to_visit.append(entry)
+print("done")
