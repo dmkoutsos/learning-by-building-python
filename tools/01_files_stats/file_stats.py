c292fe3774ffc34f28bb403915f80f106a075ccb
Dimitrios Koutsos <80091264+dmkoutsos@users.noreply.github.com.>
2025-12-25 21:11:26 +0200

    Add recursive file stats with --files flag

--- tools/01_files_stats/file_stats.py
+++ tools/01_files_stats/file_stats.py
@@ -4,18 +4,30 @@
 
 from pathlib import Path
 import sys
+
+files_only = False
+
 if len(sys.argv) == 1:
 	root = Path(".")
-else:
-	root = Path(sys.argv[1])
+elif len(sys.argv) == 2:
+	if sys.argv[1] == "--files":
+		root = Path(".")
+		files_only = True
+	else:
+		root = Path(sys.argv[1])
+else: 
+	print("Unsupported feature for now")
+
+
 to_visit = [root]
 file_count = 0
 dirs_visited = 0
 line_count = 0
 skipped_files = 0
+
+
 while to_visit:
 	current_dir = to_visit.pop()
-	print(current_dir)
 	for entry in current_dir.iterdir():
 		if (entry.is_dir() and entry.name!=".git"):
 			to_visit.append(entry)
@@ -23,12 +35,18 @@ while to_visit:
 		elif entry.is_file():
 			file_count += 1
 			try:
-				p = entry
-				with p.open() as f:
+				with entry.open() as f:
 					for line in f:
 						line_count += 1
 			except: skipped_file += 1
-print(f"Total files (recursive, no .git): {file_count}")
-print(f"Total directories visited (recursive, no .git): {dirs_visited}")
-print(f"Total lines (recursive, no .git): {line_count}")
-print(f"Total files skipped: {skipped_files}")
+
+
+if files_only == True:
+	print(f"Total files (recursive, no .git): {file_count}")
+	print(f"Total files skipped: {skipped_files}")
+else:
+	print(f"Total files (recursive, no .git): {file_count}")
+	print(f"Total directories visited (recursive, no .git): {dirs_visited}")
+	print(f"Total lines (recursive, no .git): {line_count}")
+	print(f"Total files skipped: {skipped_files}")
+
